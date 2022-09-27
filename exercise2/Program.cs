7ef863cd377d5624932f11f8ab8d179432d5b908
Nisan Erez <nisan113377@gmail.com>
2022-09-27 13:56:41 +0300

    Add project files.

---
+++ exercise2/Program.cs
@@ -0,0 +1,98 @@
+﻿using System;
+
+namespace exercise2
+{
+    internal class Program
+    {
+        static Random random = new Random();
+        static void Main(string[] args)
+        {
+            Set set1 = new Set();
+            Set set2 = new Set();
+            Set set3 = new Set();
+
+            //print the sets
+            set1 = CreatNewSet();
+            Console.WriteLine($"The set1 array:\n " + set1.ToString());
+            set2 = CreatNewSet();
+            Console.WriteLine($"The set2 array:\n {set2}");
+           
+            //Union set1 + set2
+            set1.Union(set2);
+            Console.WriteLine($"The set1 array after Union with set2:\n {set1}\n");
+
+            //The two sets after regenration
+            set1 = CreatNewSet();
+            Console.WriteLine($"The set1 array after regenration:\n {set1}");
+            set2 = CreatNewSet();
+            Console.WriteLine($"The set2 array after regenration:\n {set2}\n");
+
+            //Set1 after Intersection with set2
+            set1.Intersect(set2);
+            Console.WriteLine($"Set1 after intersection with set2, if there are similar numbers.if not, set1 will remain blank:\n {set1}\n");
+
+            //The 2 sets after regenration
+            set1 = CreatNewSet();
+            Console.WriteLine($"The set1 array after regenration:\n {set1}");
+            set2 = CreatNewSet();
+            Console.WriteLine($"The set2 array after regenration:\n {set2}");
+
+            //new set & cheking if set3 is Subset of set1
+            set3 = CreatUserSet(3);
+            Console.WriteLine($"The set3 array after regenration:\n {set3}");
+            Console.WriteLine("Set3 is Subset of set1?"); ;
+            Console.WriteLine(set1.Subset(set3));
+            Console.WriteLine();
+
+            //check if the input number IsMemeber in set1 or set2 
+            Console.WriteLine("Please choose a number to check if he exists in set1 & set2:\n");
+            int num = int.Parse(Console.ReadLine());
+            Console.WriteLine($"Does the number {num} exist In set1?\n " + set1.IsMemeber(num));
+            Console.WriteLine($"Does the number {num} exist In set 2?\n " + set2.IsMemeber(num));
+
+            //Insert a new number to the array
+            Console.WriteLine("Please choose a number, to add to set1 and to set2:");
+            int num2 = int.Parse(Console.ReadLine());
+            set1.Insert(num2);
+            set2.Insert(num2);
+            Console.WriteLine("Set 1 after adding the new number:\n " + set1);
+            Console.WriteLine("Set 2 after adding the new number:\n " + set2);
+
+            //Delete the choosen numbers from the sets
+            Console.WriteLine("Please choose a number to delete from set1 & set2:");
+            int num3 = int.Parse(Console.ReadLine());
+            set1.Delete(num3);
+            set2.Delete(num3);
+            Console.WriteLine("Set 1 after deleting the new number:\n " + set1);
+            Console.WriteLine("S2et 2 after deleting the new number:\n " + set2);
+
+            Console.ReadLine();
+        }
+
+        static Set CreatNewSet(int length = 12) 
+        {
+            Set set = new Set();
+            for (int i = 0; i < length; i++)
+            {
+                int rnd = random.Next(1000);
+                if (set.IsMemeber(rnd))
+                    i--;
+                else
+                    set.Insert(rnd);
+            }
+            return set;
+        }
+
+        public static Set CreatUserSet(int numOfUsersArray) 
+        {
+            Set set = new Set();
+            Console.WriteLine($"Please enter {numOfUsersArray} numbers, press Enter between them:");
+            for (int i = 0; i < numOfUsersArray; i++)
+            {
+                set.Insert(int.Parse(Console.ReadLine()));
+            }
+            return set;
+
+        }
+    }
+}
---
+++ exercise2/Set.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace exercise2
+{
+    public class Set
+    {
+        private bool[] arr;
+        public Set()
+        {
+            arr = new bool[1001];
+        }
+        public Set(params int[] values) : this() 
+        {
+            for (int i = 0; i < values.Length; i++)
+            {
+                if (ValidInput(values[i]))
+                    arr[values[i]] = true; //this.insert(value);
+            }
+        }
+        public void Union(Set other) //when at least one value is true
+        {
+            for (int i = 0; i < arr.Length; i++)
+            {
+                this.arr[i] = arr[i] || other.arr[i]; //if  this.arr[i] && other[i] = true; so push to the array
+                                                      //if  this.arr[i] && other[i] = false; so dont push to the array
+                                                      //if  this.arr[i] || other[i] = true; so dont push to the array
+            }
+        }
+        public void Intersect(Set other)//when both values are true
+        {
+            for (int i = 0; i < arr.Length; i++)
+            {
+                this.arr[i] = arr[i] && other.arr[i];//to push the indexs to the array
+            }
+        }
+        public bool Subset(Set other) //check if 'otherSet' is part of the 'thisSet' 
+        {
+            for (int i = 0; i < other.arr.Length; i++)
+            {
+                if (!arr[i] && other.arr[i]) //if (arr[i] == false && other.arr[i] == true)
+                    return false;
+            }
+            return true;
+        }
+        public bool IsMemeber(int number) // check if the number that we got is 'on' in the array. 
+        {
+            if (!ValidInput(number))
+                return false;
+            if (this.arr[number] == true)
+                return true;
+            return false;
+        }
+        public void Insert(int number)// add a number to the array
+        {
+            if (ValidInput(number))
+                this.arr[number] = true;
+        }
+        public void Delete(int number)
+        {
+            if (ValidInput(number))
+                this.arr[number] = false;
+        }
+        public override string ToString()
+        {
+            string text = "";
+            for (int i = 0; i < arr.Length; i++)
+            {
+                if (arr[i])
+                {
+                    text += $"{i} ";
+                }
+            }
+            return text;
+        }
+        private bool ValidInput(int number)// check if the number is in range 
+        {
+            if (number > 1000 || number < 0)
+                return false;
+            return true;
+        }
+        public override bool Equals(object obj)
+        {
+            Set otherSet = (Set)obj;
+            for (int i = 0; i < arr.Length; i++)
+            {
+                if (arr[i] != otherSet.arr[i])
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+    }
+}
